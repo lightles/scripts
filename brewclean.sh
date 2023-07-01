2c95f8fca456502ae9f81d00fd6531bae25d90cc
Steven Lightle <lightlesteven47@gmail.com>
2023-06-30 21:09:21 -0500

    updated

--- brewclean.sh
+++ brewclean.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-brew update;
+#!/opt/homebrew/bin/zsh
+echo "Updating brew..."
 brew upgrade;
+brew update;
+echo "\nCleaning up..."
 brew cleanup -s;
 brew doctor;
 brew missing;
-echo "Cleanup Finished!" | terminal-notifier -sound default
+
+echo "\nUpdating zsh packages..."
+cd $ZSH_CONFIG_DIR/plugins/ && for i in */.git; do ( echo ; echo $i; cd $i/..; git pull; ); done
+
+echo "\nCleanup Finished!" | terminal-notifier -sound default
